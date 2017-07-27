7e09b3996221d5d49dbe0a107ddf961f99325965
ice_tana <dookdevel@gmail.com>
2017-07-27 11:40:23 +0700

    edit EnterTransferId => send id to next screen

--- App/Screen/EnterTransferIdScreen.js
+++ App/Screen/EnterTransferIdScreen.js
@@ -14,15 +14,17 @@ var { height, width } = Dimensions.get('window');
     return (
       <View style={styles.container}>
         <View style={styles.top_container} >
-          <Text style={styles.text_bold}>{params.userId}</Text>
+          <Text style={styles.text_bold}>Receiver Number</Text>
             <TextInput
               keyboardType='numeric'
               style={styles.text}
-              placeholder="Input RecieverID"
+              placeholder='Input Receiver Number'
+              onChangeText={(receiverId) => this.setState({receiverId})}
+              
           />
         </View >
         <View style={styles.bottom_container}>
-         <TouchableOpacity  onPress={() => navigate('TransferCheckReceiver', { user: 'Lucy' })}>
+         <TouchableOpacity  onPress={() => navigate('TransferCheckReceiver', { userId: params.userId,receiverId: this.state.receiverId })}>
           <View style={styles.button}>
             <Text style={styles.text}>Enter</Text>
           </View>
